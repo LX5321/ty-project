06dbc8347f59c791256a916c40167096cd92d037
LX5321 <alexanderwebsite5321@gmail.com>
2019-12-28 22:10:27 +0530

    Added KNN Source code

---
+++ knn.py
@@ -0,0 +1,14 @@
+from sklearn.neighbors import KNeighborsClassifier
+training_accuracy = []
+test_accuracy = []
+neighbors_settings = range(1, 11)
+
+for n_neighbors in neighbors_settings:
+    # build the model
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+    # train the model using the train data
+    knn.fit(X_train, y_train)
+    # record training set accuracy
+    training_accuracy.append(knn.score(X_train, y_train))
+    # record test set accuracy
+    test_accuracy.append(knn.score(X_test, y_test))
